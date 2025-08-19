05456ff2ea985649af4c0bbc1e978e67f043c3ab
Mashnin Egor <mashnin_yegor@mail.ru>
2025-08-19 17:46:44 +0300

    Initial Commit

---
+++ HockeyApp/HockeyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyAppApp.swift
+//  HockeyApp
+//
+//  Created by Yoji on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HockeyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
